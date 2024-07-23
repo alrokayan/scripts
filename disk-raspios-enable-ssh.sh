254915d5b9cc8408c62d6b82e3e3bbe0a9b5b53c
alrokayan <mohammed@alrokayan.com>
2024-07-23 20:40:16 +0300

    BASE_PATH

--- disk-raspios-enable-ssh.sh
+++ disk-raspios-enable-ssh.sh
@@ -34,12 +34,21 @@ fi
 TMP_MNT_PATH="tmp/raspios"
 sudo diskutil unmountDisk "$1"
 if mkdir "$TMP_MNT_PATH"; then
-    mkdir -p tmp
-    cd tmp || exit
-    git clone https://github.com/gerard/ext4fuse.git
-    cd ext4fuse || exit
-    make
-    ./ext4fuse "$1" $TMP_MNT_PATH -o allow_other
+    BASE_PATH=$(printf "%q\n" "$(pwd)")
+    if ! [ -f tmp/ext4fuse/ext4fuse ]; then
+        mkdir -p tmp
+        cd tmp || exit
+        curl https://pkgconfig.freedesktop.org/releases/pkg-config-0.29.tar.gz -o pkgconfig.tgz
+        tar -zxf pkgconfig.tgz
+        cd pkg-config-0.29 || exit
+        ./configure && make install
+        cd "$BASE_PATH/tmp" || exit
+        git clone https://github.com/gerard/ext4fuse.git
+        cd ext4fuse || exit
+        make
+    fi
+    cd "$BASE_PATH" || exit
+    ./ext4fuse/ext4fuse "$1" $TMP_MNT_PATH -o allow_other
     touch "$TMP_MNT_PATH/ssh"
     sudo diskutil unmountDisk "$1"
     rm -rf "$TMP_MNT_PATH"
