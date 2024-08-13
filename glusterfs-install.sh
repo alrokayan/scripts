ab9ad95a27097838b908105e1e3052c6fc446cfb
alrokayan <mohammed@alrokayan.com>
2024-08-13 15:19:45 +0300

    renaming

--- glusterfs-install.sh
+++ glusterfs-install.sh
@@ -32,6 +32,13 @@ if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ "$1" = "-h" ] || [ "$1" = "--h
     fi
     exit 1
 fi
+SERVER1_NAME=$(grep $1 /etc/hosts | awk '{print $2}')
+SERVER2_NAME=$(grep $2 /etc/hosts | awk '{print $2}')
+SERVER3_NAME=$(grep $3 /etc/hosts | awk '{print $2}')
+if [ -z "$SERVER1_NAME" ] || [ -z "$SERVER2_NAME" ] || [ -z "$SERVER3_NAME" ]; then
+    echo "/etc/hosts does not contain the following IPs: $1 $2 $3"
+    exit 1
+fi
 apt install xfsprogs glusterfs-server glusterfs-client -y
 systemctl enable glusterd
 systemctl start glusterd
@@ -40,11 +47,36 @@ gluster peer probe "$2"
 gluster peer probe "$3"
 gluster peer status
 gluster pool list
+systemctl stop glusterd.service
+cd /var/lib/glusterd/vols/gfs/ || exit
+mv "gfs.$1.mnt-gfs_disk-brick1.vol" "gfs.$SERVER1_NAME.mnt-gfs_disk-brick1.vol"
+mv "gfs.$2.mnt-gfs_disk-brick1.vol" "gfs.$SERVER2_NAME.mnt-gfs_disk-brick1.vol"
+mv "gfs.$3.mnt-gfs_disk-brick1.vol" "gfs.$SERVER3_NAME.mnt-gfs_disk-brick1.vol"
+cd /var/lib/glusterd/vols/gfs/bricks || exit
+mv "$1:-mnt-gfs_disk-brick1" "$SERVER1_NAME:-mnt-gfs_disk-brick1"
+mv "$2:-mnt-gfs_disk-brick1" "$SERVER2_NAME:-mnt-gfs_disk-brick1"
+mv "$3:-mnt-gfs_disk-brick1" "$SERVER3_NAME:-mnt-gfs_disk-brick1"
+cd /var/lib/glusterd || exit
+find . -type f -exec sed -i "s/$1/$SERVER1_NAME/g" {} \;
+find . -type f -exec sed -i "s/$2/$SERVER2_NAME/g" {} \;
+find . -type f -exec sed -i "s/$3/$SERVER3_NAME/g" {} \;
+grep -rnw . -e "$1"
+grep -rnw . -e "$2"
+grep -rnw . -e "$3"
+systemctl enable -now glusterd.service
+systemctl status glusterd.service
+gluster peer status
 gluster volume create gfs replica 3 arbiter 1 transport tcp \
   "$1":/mnt/gfs_disk/brick1 \
   "$2":/mnt/gfs_disk/brick1 \
   "$3":/mnt/gfs_disk/brick1 \
   force
 gluster volume start gfs
+gluster volume heal gfs full
+gluster volume heal gfs info
+gluster volume quota gfs enable
 gluster volume status
 gluster volume info
+gluster volume status
+gluster volume info
+
