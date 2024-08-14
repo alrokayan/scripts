b3e9bca0259392f2c06edadfa2a26f3c96b2732a
Dr Mo <mohammed@alrokayan.com>
2024-08-14 23:04:50 +0300

    smb

--- glusterfs-install.sh
+++ glusterfs-install.sh
@@ -21,26 +21,44 @@
 # OR
 # curl -fL -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/alrokayan/scripts/main/glusterfs-install.sh | bash -s -- 192.168.0.2 192.168.0.3 192.168.0.4
 # $1 Server1 IP
+# $2 Server2 IP
+# $3 Server3 IP
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+    echo "Usage: $0 <Server1 IP> <Server2 IP> <Server3 IP>"
+    echo "EXAMPLE: $0 192.168.0.2 192.168.0.3 192.168.0.4"
+    if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+        echo "This script will install glusterfs"
+        exit 0
+    fi
+    exit 1
+fi
 function createGFS {
     echo "GFS_VOLUME: ${GFS_VOLUME}"
-    echo "SERVER1_NAME: $SERVER1_NAME"
-    echo "SERVER1_IP: $SERVER1_IP"
-    gluster peer probe "$SERVER1_IP"
-    gluster peer status
-    gluster pool list
-    gluster volume create ${GFS_VOLUME} \
-                          "$SERVER1_IP":/mnt/${GFS_VOLUME}_disk/brick \
-                          force
+    gluster volume create ${GFS_VOLUME} replica 3 arbiter 1 transport tcp \
+    "$SERVER1_IP":/mnt/${GFS_VOLUME}_disk/brick \
+    "$SERVER2_IP":/mnt/${GFS_VOLUME}_disk/brick \
+    "$SERVER3_IP":/mnt/${GFS_VOLUME}_disk/brick \
+    force
     gluster volume start ${GFS_VOLUME}
     systemctl stop glusterd
     cd /var/lib/glusterd/vols/${GFS_VOLUME}/ || exit
     mv "${GFS_VOLUME}.$SERVER1_IP.mnt-${GFS_VOLUME}_disk-brick.vol" "${GFS_VOLUME}.$SERVER1_NAME.mnt-${GFS_VOLUME}_disk-brick.vol"
+    mv "${GFS_VOLUME}.$SERVER2_IP.mnt-${GFS_VOLUME}_disk-brick.vol" "${GFS_VOLUME}.$SERVER2_NAME.mnt-${GFS_VOLUME}_disk-brick.vol"
+    mv "${GFS_VOLUME}.$SERVER3_IP.mnt-${GFS_VOLUME}_disk-brick.vol" "${GFS_VOLUME}.$SERVER3_NAME.mnt-${GFS_VOLUME}_disk-brick.vol"
     cd /var/lib/glusterd/vols/${GFS_VOLUME}/bricks || exit
     mv "$SERVER1_IP:-mnt-${GFS_VOLUME}_disk-brick" "$SERVER1_NAME:-mnt-${GFS_VOLUME}_disk-brick"
+    mv "$SERVER2_IP:-mnt-${GFS_VOLUME}_disk-brick" "$SERVER2_NAME:-mnt-${GFS_VOLUME}_disk-brick"
+    mv "$SERVER3_IP:-mnt-${GFS_VOLUME}_disk-brick" "$SERVER3_NAME:-mnt-${GFS_VOLUME}_disk-brick"
     cd /var/lib/glusterd || exit
     find . -type f -exec sed -i "s/$SERVER1_IP/$SERVER1_NAME/g" {} \;
+    find . -type f -exec sed -i "s/$SERVER2_IP/$SERVER2_NAME/g" {} \;
+    find . -type f -exec sed -i "s/$SERVER3_IP/$SERVER3_NAME/g" {} \;
     grep -rnw . -e "$SERVER1_IP"
+    grep -rnw . -e "$SERVER2_IP"
+    grep -rnw . -e "$SERVER3_IP"
     grep -rnw . -e "$SERVER1_NAME"
+    grep -rnw . -e "$SERVER2_NAME"
+    grep -rnw . -e "$SERVER3_NAME"
     systemctl enable --now glusterd
     systemctl status glusterd -l --no-pager
     gluster peer status
@@ -52,6 +70,8 @@ apt upgrade -y
 apt install samba -y
 apt install ctdb -y
 apt install glusterfs-server -y
+systemctl enable --now glusterd
+systemctl status glusterd -l --no-pager
 cat << EOF > /var/lib/glusterd/groups/my-samba
 cluster.self-heal-daemon=enable
 performance.cache-invalidation=on
@@ -74,22 +94,42 @@ cluster.metadata-self-heal=on
 cluster.entry-self-heal=on
 cluster.force-migration=disable
 EOF
-systemctl enable --now glusterd
-systemctl status glusterd -l --no-pager
-GFS_VOLUME="gfs"
 SERVER1_IP=$1
+SERVER2_IP=$2
+SERVER3_IP=$3
 SERVER1_NAME=$(grep "$SERVER1_IP" /etc/hosts | awk '{print $2}')
+SERVER2_NAME=$(grep "$SERVER2_IP" /etc/hosts | awk '{print $2}')
+SERVER3_NAME=$(grep "$SERVER3_IP" /etc/hosts | awk '{print $2}')
+echo "SERVER1_NAME: $SERVER1_NAME"
+echo "SERVER1_IP: $SERVER1_IP"
+echo "SERVER2_NAME: $SERVER2_NAME"
+echo "SERVER2_IP: $SERVER2_IP"
+echo "SERVER3_NAME: $SERVER3_NAME"
+echo "SERVER3_IP: $SERVER3_IP"
+if [ -z "$SERVER1_NAME" ] || [ -z "$SERVER2_NAME" ] || [ -z "$SERVER3_NAME" ]; then
+    echo "/etc/hosts does not contain the following IPs: $SERVER1_IP $SERVER2_IP $SERVER3_IP"
+    exit 1
+fi
+gluster peer probe "$SERVER1_IP"
+gluster peer probe "$SERVER2_IP"
+gluster peer probe "$SERVER3_IP"
+gluster peer status
+gluster pool list
+GFS_VOLUME="gfs"
 createGFS
 gluster volume set ${GFS_VOLUME} group my-samba
-gluster volume set ${GFS_VOLUME} storage.batch-fsync-delay-usec 0
 GFS_VOLUME="ctdb"
 sed -i 's/META="all"/META="ctdb"/g' /var/lib/glusterd/hooks/1/start/post/S29CTDBsetup.sh
 sed -i 's/META="all"/META="ctdb"/g' /var/lib/glusterd/hooks/1/stop/pre/S29CTDB-teardown.sh
 createGFS
 cat << EOF > /etc/ctdb/nodes
-10.0.0.12
+$SERVER1_IP
+$SERVER2_IP
+$SERVER3_IP
 EOF
 cat << EOF > /etc/ctdb/public_addresses
+10.10.1.10/16 eno1
+10.10.1.11/16 eno1
 10.10.1.12/16 eno1
 EOF
 sed -i '/CTDB_SAMBA_SKIP_SHARE_CHECK/d' /etc/ctdb/script.options
@@ -111,5 +151,3 @@ store dos attributes = yes
 EOF
 systemctl enable --now samba-ad-dc
 systemctl status samba-ad-dc -l --no-pager
-
-
