e0fd4bf7d7ade03f921a3852fd73f1d83b0a180b
Dr Mo <mohammed@alrokayan.com>
2024-08-19 05:46:06 +0300

    gfs

--- glusterfs-install.sh
+++ glusterfs-install.sh
@@ -17,20 +17,18 @@
 # under the License.
 #
 # HOW TO:
-# rm -r scripts && git clone https://github.com/alrokayan/scripts.git && cd scripts && chmod +x * && ./glusterfs-install.sh 192.168.0.2 192.168.0.3 192.168.0.4 10.10.10.10/24 10.10.10.11/24 10.10.10.12/24 eth0 sdb
+# rm -r scripts && git clone https://github.com/alrokayan/scripts.git && cd scripts && chmod +x * && ./glusterfs-install.sh 192.168.0.2 192.168.0.3 192.168.0.4 10.10.10.10/24 eth0 sdb
 # OR
-# curl -fL -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/alrokayan/scripts/main/glusterfs-install.sh | bash -s --192.168.0.2 192.168.0.3 192.168.0.4 10.10.10.10/24 10.10.10.11/24 10.10.10.12/24 eth0 sdb
+# curl -fL -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/alrokayan/scripts/main/glusterfs-install.sh | bash -s --192.168.0.2 192.168.0.3 192.168.0.4 10.10.10.10/24 eth0 sdb
 # $1 Server1 IP
 # $2 Server2 IP
 # $3 Server3 IP
-# $4 SERVER1 PUBLIC IP WITH CIDR
-# $5 SERVER2 PUBLIC IP WITH CIDR
-# $6 SERVER3 PUBLIC IP WITH CIDR
-# $7 NETWORK INTERFACE CARD FOR PUBLIC IPs
-# $8 Disk (without /dev)
-if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
-    echo "Usage: $0 <Server1 IP> <Server2 IP> <Server3 IP> <Server1 Public IP with CIDR> <Server2 Public IP with CIDR> <Server3 Public IP with CIDR> <Network Interface Card> <Disk to wipe>"
-    echo "EXAMPLE: $0 192.168.0.2 192.168.0.3 192.168.0.4 10.10.10.10/24 10.10.10.11/24 10.10.10.12/24 eth0 sdb"
+# $4 SERVER PUBLIC IP WITH CIDR
+# $5 NETWORK INTERFACE CARD FOR PUBLIC IPs
+# $6 Disk (without /dev)
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ] || [ -z "$5" ] || [ -z "$6" ] || [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+    echo "Usage: $0 <Server1 IP> <Server2 IP> <Server3 IP> <Server Public IP with CIDR> <Network Interface Card> <Disk to wipe>"
+    echo "EXAMPLE: $0 192.168.0.2 192.168.0.3 192.168.0.4 10.10.10.10/24 eth0 sdb"
     if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
         echo "This script will install glusterfs"
         exit 0
@@ -40,11 +38,9 @@ fi
 SERVER1_IP=$1
 SERVER2_IP=$2
 SERVER3_IP=$3
-SERVER1_IP_PUBLIC_CIDR=$4
-SERVER2_IP_PUBLIC_CIDR=$5
-SERVER3_IP_PUBLIC_CIDR=$6
-NIC=$7
-DISK=$8
+SERVER_IP_PUBLIC_CIDR=$4
+NIC=$5
+DISK=$6
 function createGFS {
     echo "GFS_VOLUME: ${GFS_VOLUME}"
     if ! gluster volume info ${GFS_VOLUME} &>/dev/null; then
@@ -73,25 +69,27 @@ apt install samba -y
 apt install ctdb -y
 cat << EOF > /var/lib/glusterd/groups/my-samba
 cluster.self-heal-daemon=enable
-performance.cache-invalidation=on
-server.event-threads=4
-client.event-threads=4
-performance.parallel-readdir=on
-performance.readdir-ahead=on
-performance.nl-cache-timeout=600
-performance.nl-cache=on
-network.inode-lru-limit=200000
-performance.md-cache-timeout=600
-performance.stat-prefetch=on
-performance.cache-samba-metadata=on
-features.cache-invalidation-timeout=600
-features.cache-invalidation=on
-nfs.disable=on
-user.smb=enable
 cluster.data-self-heal=on
 cluster.metadata-self-heal=on
 cluster.entry-self-heal=on
 cluster.force-migration=disable
+performance.cache-invalidation=on
+performance.parallel-readdir=on
+performance.readdir-ahead=on
+performance.nl-cache-timeout=600
+performance.nl-cache=on
+performance.md-cache-timeout=600
+performance.stat-prefetch=on
+performance.cache-samba-metadata=on
+performance.write-behind=off
+features.cache-invalidation-timeout=600
+features.cache-invalidation=on
+server.event-threads=4
+client.event-threads=4
+nfs.disable=on
+user.smb=enable
+network.inode-lru-limit=200000
+storage.batch-fsync-delay-usec=0
 EOF
 SERVER1_NAME=$(grep "$SERVER1_IP" /etc/hosts | awk '{print $2}')
 SERVER2_NAME=$(grep "$SERVER2_IP" /etc/hosts | awk '{print $2}')
@@ -137,9 +135,7 @@ $SERVER2_IP
 $SERVER3_IP
 EOF
 cat << EOF > /etc/ctdb/public_addresses
-$SERVER1_IP_PUBLIC_CIDR $NIC
-$SERVER2_IP_PUBLIC_CIDR $NIC
-$SERVER3_IP_PUBLIC_CIDR $NIC
+$SERVER_IP_PUBLIC_CIDR $NIC
 EOF
 sed -i '/CTDB_SAMBA_SKIP_SHARE_CHECK/d' /etc/ctdb/script.options
 echo 'CTDB_SAMBA_SKIP_SHARE_CHECK=yes' >> /etc/ctdb/script.options
@@ -148,18 +144,19 @@ systemctl status ctdb -l --no-pager
 ctdb status
 ctdb ip
 ctdb ping
+df -h /gluster/lock
+./scripts/glusterfs-client.sh
+mkdir /gfs/smbshare
+groupadd smbgroup
+chgrp smbgroup /gfs/smbshare
+usermod -aG smbgroup root
+chmod 770 /gfs/smbshare
+umount /gfs
 cp /etc/samba/smb.conf /etc/samba/smb.conf.ORIGINAL
-cat << EOF >> /etc/samba/smb.conf
-kernel share modes = no
-kernel oplocks = no
-map archive = no
-map hidden = no
-map read only = no
-map system = no
-store dos attributes = yes
-EOF
-systemctl enable --now ctdb
-systemctl status ctdb -l --no-pager
-systemctl enable --now samba-ad-dc
-systemctl start samba-ad-dc
-systemctl status samba-ad-dc -l --no-pager
+AddGlobalSMB='[global]\n    kernel share modes = no\n    kernel oplocks = no\n    map archive = no\n    map hidden = no\n    map read only = no\n    map system = no\n    store dos attributes = yes'
+sed -i "/\[global\]/c $AddGlobalSMB" /etc/samba/smb.conf
+AddGFSSMB='[gluster-gfs]\n    writable = yes\n    valid users = @smbgroup\n    force create mode = 777\n    force directory mode = 777\n    inherit permissions = yes'
+sed -i "/\[gluster-gfs\]/c $AddGFSSMB" /etc/samba/smb.conf
+systemctl restart smbd
+systemctl status smbd -l --no-pager
+ufw allow samba
